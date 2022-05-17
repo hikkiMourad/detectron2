8e377f44e7292072456c6313aa3a404f48fc91db
hikkiMourad <hikki.mourad@gmail.com>
2022-05-17 20:20:29 +0100

    added brainSeg to detectron2 projects

--- detectron2/projects/__init__.py
+++ detectron2/projects/__init__.py
@@ -6,6 +6,7 @@ _PROJECTS = {
     "point_rend": "PointRend",
     "deeplab": "DeepLab",
     "panoptic_deeplab": "Panoptic-DeepLab",
+    "brainSeg" : "BrainSeg-rcnn"
 }
 _PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent / "projects"
 
